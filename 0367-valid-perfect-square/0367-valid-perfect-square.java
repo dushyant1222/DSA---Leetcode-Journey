f8ee98319ba6fb77ac3b4f2d572465d52421cdec
Dushyant Kumar <149196558+dushyant1222@users.noreply.github.com>
2025-01-05 20:01:09 +0530

    Time: 2 ms (13.13%), Space: 40.8 MB (15.72%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.java
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -1,10 +1,7 @@
 class Solution {
     public boolean isPerfectSquare(int num) {
         for(double i = 1; i*i<= num; i++){
-            if(i * i == num) {
-                if((num%i ==0) && (num/i == i)) return true;
-                
-            }
+            if(i * i == num) return true;
         }
         return false;
     }
