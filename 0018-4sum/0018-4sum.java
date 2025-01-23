4343551a00f21bd5891a4b41b041f9f386e6d7e1
Dushyant Kumar <149196558+dushyant1222@users.noreply.github.com>
2025-01-23 21:49:32 +0530

    Time: 3 ms (99.72%), Space: 43.9 MB (64.56%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,17 +1,21 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         List<List<Integer>> arr = new ArrayList<>();
-
-        if(nums == null || nums.length < 4) return arr;
+        int n = nums.length;
+        if(nums == null || n < 4) return arr;
         Arrays.sort(nums);
-        for(int i = 0; i< nums.length - 3; i++){
+        for(int i = 0; i< n - 3; i++){
             if(i>0 && nums[i] == nums[i - 1]) continue;
+            if((long) nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target) break;
+            if((long) nums[i] + nums[n - 1] + nums[n - 2] + nums[n - 3] < target) continue;
 
-            for(int j = i + 1; j<nums.length - 2; j++){
+            for(int j = i + 1; j< n - 2; j++){
             if(j > i+1 && nums[j] == nums[j - 1 ]) continue;
+            if((long) nums[i] + nums[j] + nums[j + 1] + nums[j + 2] > target) break;
+            if((long) nums[i] + nums[j] + nums[n - 1] + nums[n - 2] < target) continue;
 
             int start = j + 1;
-            int end = nums.length - 1;
+            int end = n - 1;
 
             while(start < end){
                 long sum = (long) nums[i] + nums[j] + nums[start] + nums[end];
