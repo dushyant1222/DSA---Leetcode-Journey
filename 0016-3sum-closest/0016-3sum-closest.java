403b774e4b34832c426f0106477e7379cae4ddbb
Dushyant Kumar <149196558+dushyant1222@users.noreply.github.com>
2025-02-07 22:42:33 +0530

    Time: 19 ms (11.51%), Space: 43.9 MB (5.25%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -4,13 +4,18 @@ class Solution {
         int min = Integer.MAX_VALUE / 2;
 
         for(int i = 0; i < nums.length - 2 ;i ++){
+            if(i > 0  && nums[i] == nums[i - 1]) continue;
             int start = i + 1, end = nums.length - 1;
             while(start < end){
                 int temp = nums[i] + nums[start] + nums[end];
                 if(Math.abs(temp - target) < Math.abs(min - target)) min = temp;
                 if(temp == target) return temp;
-                else if(target < temp) --end;
-                else ++start;
+                else if(target < temp){
+                    end--; while(start < end && nums[end] == nums[end + 1]) end--;
+                }
+                else{
+                    start++; while(start < end && nums[start] == nums[start - 1]) start++;
+                }
             }
         }
         return min;
