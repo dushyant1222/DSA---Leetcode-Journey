dd3e2e4d77093dc809775f007de34c6d4b60ba0e
Dushyant Kumar <149196558+dushyant1222@users.noreply.github.com>
2025-02-10 22:24:43 +0530

    Time: 9 ms (74.77%), Space: 46.1 MB (41.63%) - LeetHub

--- 0924-fair-candy-swap/0924-fair-candy-swap.java
+++ 0924-fair-candy-swap/0924-fair-candy-swap.java
@@ -1,8 +1,12 @@
 class Solution {
     public int[] fairCandySwap(int[] aliceSizes, int[] bobSizes) {
         int alicesum = 0 , bobsum = 0;
-        for(int i: aliceSizes) alicesum += i;
-        for(int j: bobSizes) bobsum += j;
+        for(int i: aliceSizes) {
+        alicesum += i;
+        }
+        for(int j: bobSizes){
+             bobsum += j;
+        }
 
         int diff = (alicesum - bobsum)/2;
 
@@ -11,7 +15,9 @@ class Solution {
 
         for(int b: bobSizes){
             int a = b + diff;
-            if(aliceset.contains(a)) return new int[]{a,b};
+            if(aliceset.contains(a)) {
+              return new int[]{a,b};
+            }
         }
         return new int[]{-1,-1};
     }
