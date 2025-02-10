db91da37a549ae3a5aae6b09068b6bae6858f7ef
Dushyant Kumar <149196558+dushyant1222@users.noreply.github.com>
2025-02-10 22:28:24 +0530

    Time: 10 ms (69.91%), Space: 46 MB (58.04%) - LeetHub

--- 0924-fair-candy-swap/0924-fair-candy-swap.java
+++ 0924-fair-candy-swap/0924-fair-candy-swap.java
@@ -1,7 +1,8 @@
 class Solution {
     public int[] fairCandySwap(int[] aliceSizes, int[] bobSizes) {
-        int alicesum = Arrays.stream(aliceSizes).sum();
-        int bobsum = Arrays.stream(bobSizes).sum();
+        int alicesum = 0 , bobsum = 0;
+        for(int i: aliceSizes) alicesum += i;
+        for(int j: bobSizes) bobsum += j;
 
         int diff = (alicesum - bobsum)/2;
 
