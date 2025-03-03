0e0700d198fffeaea95ab1ddb5ec9f69cba1886c
Dushyant Kumar <149196558+dushyant1222@users.noreply.github.com>
2025-03-03 23:12:49 +0530

    Time: 39 ms (78.36%), Space: 60.9 MB (42.54%) - LeetHub

--- 0164-maximum-gap/0164-maximum-gap.java
+++ 0164-maximum-gap/0164-maximum-gap.java
@@ -3,7 +3,7 @@ class Solution {
         if(nums.length == 1) return 0;
 
         Arrays.sort(nums);
-        int res = Integer.MIN_VALUE;
+        int res = 0;
         for(int i = 0; i < nums.length - 1; i++){
             if(res < nums[i+1] - nums[i]) res = nums[i + 1]- nums[i];
         }
