b668f6fed9ab979ac07f9fc5c513033d80ba8856
Dushyant Kumar <149196558+dushyant1222@users.noreply.github.com>
2025-08-25 19:19:23 +0530

    Time: 8 ms (19.52%), Space: 41.5 MB (95.89%) - LeetHub

--- 1823-determine-if-string-halves-are-alike/1823-determine-if-string-halves-are-alike.java
+++ 1823-determine-if-string-halves-are-alike/1823-determine-if-string-halves-are-alike.java
@@ -1,11 +1,18 @@
 class Solution {
-    String vowels = "aeiouAEIOU";
-    public boolean halvesAreAlike(String S) {
-        int mid = S.length() / 2, ans = 0;
-        for (int i = 0, j = mid; i < mid; i++, j++) {
-            if (vowels.indexOf(S.charAt(i)) >= 0) ans++;
-            if (vowels.indexOf(S.charAt(j)) >= 0) ans--;
+    public boolean halvesAreAlike(String s) {
+        String vow = "aeiou";
+        int mid = s.length() /2;
+        int count1 =0;
+        int count2 =0;
+        s=s.toLowerCase();
+        for(int i=0; i< mid; i++){
+            if(s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u') count1++;
         }
-        return ans == 0;
+        for(int i = mid; i < s.length(); i++){
+            if(s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u') count2++;
+        }
+        if(count1 == count2) return true;
+        else return false;
+
     }
 }
