2f740ab8123ecf1d72fe6c3a69b94f992bcc1d1b
Dushyant Kumar <149196558+dushyant1222@users.noreply.github.com>
2025-09-01 23:29:00 +0530

    Time: 1 ms (82.26%), Space: 41.8 MB (45.46%) - LeetHub

--- 1434-decrypt-string-from-alphabet-to-integer-mapping/1434-decrypt-string-from-alphabet-to-integer-mapping.java
+++ 1434-decrypt-string-from-alphabet-to-integer-mapping/1434-decrypt-string-from-alphabet-to-integer-mapping.java
@@ -5,11 +5,11 @@ class Solution {
         for(int i = s.length()-1; i>=0; i-- ){
             int no;
             if(s.charAt(i) == '#'){
-                no = Integer.parseInt("" + s.charAt(i-2) + s.charAt(i-1));
+                no = (s.charAt(i-2) - '0') * 10 + (s.charAt(i-1) - '0');
                 sbb.append((char)('a' + (no - 1)));
                 i-=2;
             } else { 
-                no =Integer.parseInt("" + s.charAt(i));
+                no = s.charAt(i) -'0';
                 sbb.append((char)('a' + (no-1)));
             }
         }
