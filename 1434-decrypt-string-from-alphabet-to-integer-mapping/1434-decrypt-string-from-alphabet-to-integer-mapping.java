50a00d20783c7f815e7c735b6dcb76af93f4639d
Dushyant Kumar <149196558+dushyant1222@users.noreply.github.com>
2025-09-01 23:27:27 +0530

    Time: 5 ms (20.29%), Space: 42.1 MB (28.03%) - LeetHub

--- 1434-decrypt-string-from-alphabet-to-integer-mapping/1434-decrypt-string-from-alphabet-to-integer-mapping.java
+++ 1434-decrypt-string-from-alphabet-to-integer-mapping/1434-decrypt-string-from-alphabet-to-integer-mapping.java
@@ -1,16 +1,19 @@
 class Solution {
     public String freqAlphabets(String s) {
-        StringBuilder sb = new StringBuilder();
-        
-        for(int i = s.length()-1; i >= 0; i--) {
-            int number;
-            if(s.charAt(i) == '#') { 
-                number = (s.charAt(i-2) - '0') * 10 + (s.charAt(i-1) - '0');
-                i-=2;
-            } else number = s.charAt(i) - '0';
-            sb.append((char) (number + 96));
-        }
+        StringBuilder sbb = new StringBuilder();
 
-        return sb.reverse().toString();
+        for(int i = s.length()-1; i>=0; i-- ){
+            int no;
+            if(s.charAt(i) == '#'){
+                no = Integer.parseInt("" + s.charAt(i-2) + s.charAt(i-1));
+                sbb.append((char)('a' + (no - 1)));
+                i-=2;
+            } else { 
+                no =Integer.parseInt("" + s.charAt(i));
+                sbb.append((char)('a' + (no-1)));
+            }
+        }
+        return sbb.reverse().toString();
+                             
     }
 }
